7f48abcf1eb21c3aabea9a8fc084aa27bf3da6bf
Bitzcanon <42229042+Bitzcanon@users.noreply.github.com>
2019-02-20 17:48:10 +0800

    Commented out broken code

--- DM2122Prac/Source/Music.h
+++ DM2122Prac/Source/Music.h
@@ -1,19 +1,19 @@
-#pragma once
-#define WITH_WINMM
-
-#include "../../soloud20181119/include/soloud.h"
-#include "../../soloud20181119/include/soloud_wav.h"
-
-using namespace std;
-
-class Music
-{
-public:
-	Music();
-	~Music();
-	void playBGM(int i);
-	void playInstantSound(int i);
-private:
-	SoLoud::Soloud gSoloud;
-	SoLoud::Wav gWave;
-};
+//#pragma once
+//#define WITH_WINMM
+//
+//#include "../../soloud20181119/include/soloud.h"
+//#include "../../soloud20181119/include/soloud_wav.h"
+//
+//using namespace std;
+//
+//class Music
+//{
+//public:
+//	Music();
+//	~Music();
+//	void playBGM(int i);
+//	void playInstantSound(int i);
+//private:
+//	SoLoud::Soloud gSoloud;
+//	SoLoud::Wav gWave;
+//};
