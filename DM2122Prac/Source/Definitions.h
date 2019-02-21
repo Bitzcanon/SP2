878f1ab9a41400251dcbe64f88fee201bc71a448
Bitzcanon <42229042+Bitzcanon@users.noreply.github.com>
2019-02-21 12:59:52 +0800

    Fixed Car deceleration and chase scene

--- DM2122Prac/Source/Definitions.h
+++ DM2122Prac/Source/Definitions.h
@@ -4,7 +4,7 @@
 //Done by Winston
 
 constexpr auto CAMERABOUNDSORIGINAL = 4998.f;
-constexpr auto CAMERABOUNDSCHASE = 373.5f;
+constexpr auto CAMERABOUNDSCHASE = 248.f;
 
 constexpr auto ROADBLOCKCOUNT = 9;
 constexpr auto ROADBLOCKROWCOUNT = 5; //How many elements there are in each row in the text file
@@ -12,7 +12,7 @@ constexpr auto ROADBLOCKROWCOUNT = 5; //How many elements there are in each row 
 constexpr auto CHECKPOINTCOUNT = 10;
 constexpr auto CHECKPOINTROWCOUNT = 4; //How many elements there are in each row in the text file
 
-constexpr auto MAZETILECOUNT = 48;
+constexpr auto MAZETILECOUNT = 44;
 constexpr auto MAZETILEROWCOUNT = 3;
 
 #endif // !DEFINITIONS_H
